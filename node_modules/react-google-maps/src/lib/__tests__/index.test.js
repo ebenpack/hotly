bb9cbb0e52669f1db1f33e5d19c0f89458594d51
ebenpack <ebenpackwood@gmail.com>
2017-06-25 20:57:38 -0400

    FIx paths

---
+++ node_modules/react-google-maps/src/lib/__tests__/index.test.js
@@ -0,0 +1,33 @@
+import {
+  withGoogleMap,
+
+  GoogleMap,
+  Circle,
+  DirectionsRenderer,
+  InfoWindow,
+  KmlLayer,
+  FusionTablesLayer,
+  Marker,
+  OverlayView,
+  Polygon,
+  Polyline,
+  Rectangle,
+} from "../index";
+
+describe(`index`, () => {
+  it(`should be exported`, () => {
+    expect(withGoogleMap).toBeDefined();
+
+    expect(GoogleMap).toBeDefined();
+    expect(Circle).toBeDefined();
+    expect(DirectionsRenderer).toBeDefined();
+    expect(InfoWindow).toBeDefined();
+    expect(KmlLayer).toBeDefined();
+    expect(FusionTablesLayer).toBeDefined();
+    expect(Marker).toBeDefined();
+    expect(OverlayView).toBeDefined();
+    expect(Polygon).toBeDefined();
+    expect(Polyline).toBeDefined();
+    expect(Rectangle).toBeDefined();
+  });
+});
